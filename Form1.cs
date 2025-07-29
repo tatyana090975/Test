6d1ab4571631d0d70ea6c5c31fd48d5ad13f7cd1
tatya <tatya@DESKTOP-8U280KM>
2025-07-29 15:25:25 +0400

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {  
+            Form4 form4 = new Form4();
+            form4.Show();
+            linkLabel1.LinkVisited = true;
+        }
+    }
+}
